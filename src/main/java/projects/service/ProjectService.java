b1804e82443a999973340cb175cc6fb521f16bfc
tanjilamumu <tanjilamumu@ymail.com>
2024-03-22 23:41:16 -0400

    Updating for week 11

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 	private ProjectDao projectDao = new ProjectDao();
@@ -23,6 +24,19 @@ public class ProjectService {
 				() -> new NoSuchElementException(
 			"Project with project ID=" + projectId + " does not exist"));
 		
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID =" + project.getProjectId() + "does not exist.");
+			
+		}
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID =" +projectId + "does not exist.");
+		}
 	}
 }
 	
